7008b1be91b17ceb9bf24fc1002d6b8c01f5c446
ClarkGodwin <clarkniyonzima@gmail.com>
2026-09-02 17:29:40 +0200

    user table in migration done

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->boolean('email_verified')->default(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('status', [user, admin]);
             $table->rememberToken();
             $table->timestamps();
         });
