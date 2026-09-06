07876453a37f5b4e4a5ea3680d3efb62b94c2ed4
ClarkGodwin <clarkniyonzima@gmail.com>
2026-09-06 08:38:01 +0200

    guestnavbar done

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,31 +1,22 @@
+<script lang="ts">
+export default{
+    layout: GuestNavbar,
+}
+</script>
+
 <script setup lang="ts">
 import { User } from '@/types';
-import { Head, Link } from '@inertiajs/vue3';
-import { LogIn } from '@lucide/vue';
-import { route } from 'ziggy-js';
-
+import GuestNavbar from './components/GuestNavbar.vue';
 defineProps<{ users: User[] }>()
 
 </script>
 
 <template>
-    <div class="w-welcome-navbar mx-auto mt-3">
-        <navbar class="flex justify-between ">
-            <h1 class="text-frosted font-bold text-welcome-navbar-h1">N_Todoist</h1>
-            <div class="flex gap-3 *:hover:cursor-pointer">
-                <button class="flex gap-2 items-center text-white bg-frosted px-welcome-navbar-links-px rounded-xl ">
-                    <log-in class="size-(--text-welcome-navbar-links)"/>
-                    <span class="font-bold text-welcome-navbar-links">Login</span>
-                </button>
-                <button class="font-bold text-frosted text-welcome-navbar-links underline hover:no-underline">Register</button>
-            </div>
-        </navbar>
+    <p class="mt-8 w-welcome-p text-welcome-p">
+        Welcome in this minimalistic web app to create and track tasks daily and change their status from to do, being
+        done and done and move on with your life. <br>
+        For those who can easily forget like myself ,
+        <span class="text-frosted font-medium">LOGIN AND USE IT</span>
+    </p>
 
-        <p class="mt-8 w-welcome-p text-welcome-p">
-            Welcome in this minimalistic web app to create and track tasks daily and change their status from to do, being done and done and move on with your life. <br>
-            For those who can easily forget like myself ,
-            <span class="text-frosted font-medium">LOGIN AND USE IT</span>
-        </p>
-
-    </div>
 </template>
---
+++ resources/js/pages/components/GuestNavbar.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { LogIn } from '@lucide/vue';
+
+</script>
+
+<template>
+    <div class="w-guestNavbar-navbar mx-auto mt-3">
+        <navbar class="flex justify-between ">
+            <h1 class="text-frosted font-bold text-guestNavbar-navbar-h1">N_Todoist</h1>
+            <div class="flex gap-3 *:hover:cursor-pointer">
+                <button class="flex gap-2 items-center text-white bg-frosted px-guestNavbar-navbar-links-px rounded-xl ">
+                    <log-in class="size-(--text-guestNavbar-navbar-links)"/>
+                    <span class="font-bold text-guestNavbar-navbar-links">Login</span>
+                </button>
+                <button class="font-bold text-frosted text-guestNavbar-navbar-links underline hover:no-underline">Register</button>
+            </div>
+        </navbar>
+
+        <slot></slot>
+
+    </div>
+</template>
