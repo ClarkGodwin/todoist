1693d4504ecc16b286dd180f18693cbb0d3a5cc8
ClarkGodwin <clarkniyonzima@gmail.com>
2026-09-06 20:03:34 +0200

    password reset component finished

--- resources/js/pages/auth/PasswordReset.vue
+++ resources/js/pages/auth/PasswordReset.vue
@@ -27,15 +27,5 @@ const inputs = [
 </script>
 
 <template>
-    <Form title="Login to your account" :inputs="inputs">
-        <div>
-            No account yet ?
-            <Link :href="route('register')" class="text-frosted underline hover:no-underline">Register</Link>
-        </div>
-
-        <div class="mt-3">
-            Forgot your password ?
-            <Link :href="route('password.forgotten')" class="text-frosted underline hover:no-underline">Click here</Link>
-        </div>
-    </Form>
+    <Form title="Login to your account" :inputs="inputs"/>
 </template>
