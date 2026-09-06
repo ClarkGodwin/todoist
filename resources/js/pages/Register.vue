74050e11cc42376bbd997fcfbea3809d345ffa6c
ClarkGodwin <clarkniyonzima@gmail.com>
2026-09-06 10:19:39 +0200

    register page done

--- resources/js/pages/Register.vue
+++ resources/js/pages/Register.vue
@@ -7,6 +7,8 @@ export default {
 <script setup lang="ts">
 import GuestNavbar from './components/GuestNavbar.vue';
 import Input from './components/Input.vue';
+import { Link } from '@inertiajs/vue3';
+import { route } from 'ziggy-js';
 
 const inputs = [
     {
@@ -19,14 +21,32 @@ const inputs = [
         type: 'email',
         name: 'E-mail',
     },
+    {
+        id: 3,
+        type: 'password',
+        name: 'Password',
+    },
+    {
+        id: 4,
+        type: 'password',
+        name: 'Confirm',
+    },
 ]
 
 </script>
 
 <template>
-    <div class="bg-surface-300 rounded-2xl p-4 mt-[100px] w-[600px] mx-auto flex flex-col gap-4">
+    <div class="bg-surface-300 rounded-2xl p-[20px] mt-[60px] w-[600px] mx-auto flex flex-col gap-[30px]">
+        <h2 class="text-frosted font-bold text-center text-[25px]">Register a new account</h2>
         <div v-for="input in inputs" :key="input.id">
             <Input :type="input.type" :name="input.name" />
+        </div>
+
+        <button class="bg-frosted text-white font-bold rounded-xl p-[10px]">Submit</button>
+
+        <div>
+            Already have an account ?
+            <Link :href="route('login')" class="text-frosted underline hover:no-underline">Login</Link>
         </div>
     </div>
 </template>
