af147a283ac2f1480dfee4443c7cb05c77f3b9c6
ClarkGodwin <clarkniyonzima@gmail.com>
2026-09-10 11:37:09 +0200

    displaytoast tested

--- resources/js/pages/components/Toast.vue
+++ resources/js/pages/components/Toast.vue
@@ -1,21 +1,29 @@
 <script setup lang="ts">
 import { usePage } from '@inertiajs/vue3';
 import { CircleCheck } from '@lucide/vue';
+import { ref } from 'vue';
 
 const page = usePage()
 
-console.log(page.flash.success)
+const visible = ref(true)
+
+function displayToastForTwoSeconds(){
+    setTimeout(() => {
+        visible.value = false
+
+    }, 2000);
+}
+
+displayToastForTwoSeconds()
 
 </script>
 
 <template>
-    <div class="relative left-toast-left-position ">
+    <div v-if="visible" class="relative left-toast-left-position ">
         <div
             class="font-bold text-white text-toast bg-frosted h-fit flex items-center justify-center gap-toast-gap  p-toast-p rounded-xl fixed top-toast-top-position">
             <CircleCheck class="size-(--text-toast)" />
-            <span>
-                The credentials do not match
-            </span>
+            <span>{{ page.flash.success }}</span>
         </div>
     </div>
 </template>
