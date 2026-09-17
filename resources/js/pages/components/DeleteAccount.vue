0f51e48956746be4f0fae8e3aa3756c7080bc769
ClarkGodwin <clarkniyonzima@gmail.com>
2026-09-17 20:42:18 +0200

    appearance of the delete config done

--- resources/js/pages/components/DeleteAccount.vue
+++ resources/js/pages/components/DeleteAccount.vue
@@ -1,12 +1,45 @@
 <script setup lang="ts">
 import { Trash2 } from '@lucide/vue';
 
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
+
 </script>
 
 <template>
-    <button
-        class="bg-red-500 px-3 py-1.5 rounded-lg sm:rounded-xl text-account text-white font-bold flex items-center gap-1">
-        <Trash2 class="size-(--text-account)" />
-        <span>Delete the account</span>
-    </button>
+    <AlertDialog>
+        <AlertDialogTrigger
+            class="bg-red-500 px-3 py-1.5 rounded-lg sm:rounded-xl text-account text-white font-bold flex items-center gap-1">
+            <Trash2 class="size-(--text-account)" />
+            <span>Delete the account</span>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+            <AlertDialogHeader>
+                <AlertDialogTitle>
+                    Are you absolutely sure?
+                </AlertDialogTitle>
+                <AlertDialogDescription>
+                    This action cannot be undone. This will permanently delete your account
+                    and remove your data from our servers.
+                </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter class="text-white font-bold">
+                <AlertDialogCancel class="bg-frosted" >
+                    Cancel
+                </AlertDialogCancel>
+                <AlertDialogAction class="bg-red-500">
+                    Continue
+                </AlertDialogAction>
+            </AlertDialogFooter>
+        </AlertDialogContent>
+    </AlertDialog>
 </template>
