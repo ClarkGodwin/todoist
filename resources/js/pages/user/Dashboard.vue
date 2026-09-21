bd6ea9976391742bf58892bc7ebe3589dc5f1719
ClarkGodwin <clarkniyonzima@gmail.com>
2026-09-21 11:17:43 +0200

    Dashboard's text added'

--- resources/js/pages/user/Dashboard.vue
+++ resources/js/pages/user/Dashboard.vue
@@ -8,15 +8,26 @@ export default {
 </script>
 
 <script setup lang="ts">
+import { User } from '@/types/auth.js';
 import DashboardInfo from '../components/DashboardInfo.vue';
 import UserSidebar from '../components/UserSidebar.vue';
+
+const props = defineProps<{
+    auth: {
+        user: User
+    }
+}>()
+
+const user = props.auth.user
 </script>
 
 <template>
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima quos perspiciatis incidunt laudantium autem,
-    maiores similique ratione commodi, debitis magni porro sequi qui corporis reprehenderit ullam dolores tenetur ab
-    pariatur!
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, error. Cupiditate, ipsa. Dignissimos asperiores
-    officia tempora placeat fugiat officiis soluta accusamus ex nesciunt ullam odit totam, nisi, velit aspernatur
-    doloribus!
+    <span>{{ user.name }}</span>
+    <span>
+        , congratulation, you've made it, registering an account, verifying the email and logging into that account. <br> <br>
+        Now you can enjoy the features proposed by the app, check the account page in the sidebar to check your user informations, modifying them and even deleting your account if you feel like it. <br> <br>
+        The task page is where you'll be able to enjoy the reason why you registered, you can create a task, modify it, mark it as done and even deleting it if you ever change your mind <br>
+        This app shows the task for one day, if you'd like to see the planned for another day, just click on the day on which you actually are and then select the day you want to check. <br>
+        However, you don't have to go through all that just to create a task for a specific day
+    </span>
 </template>
